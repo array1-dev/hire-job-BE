07700a8681a319fd330ffc97e1aca48f20e91fff
dhanisetiaji <dhanisetiaji99@gmail.com>
2022-08-05 22:06:43 +0700

    add feature auth

---
+++ routes/authRoute.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+const { register, verify, login, forgotPass, changePassword, verifyToken, verifyCode } = require('../controllers/authController')
+
+router.post('/register', register)
+router.post('/forgot-pass', forgotPass)
+router.post('/verify', verify)
+router.post('/login', login)
+router.patch('/change-pass', changePassword)
+router.post('/verify-token', verifyToken)
+router.get('/verify-code', verifyCode)
+
+
+module.exports = router
