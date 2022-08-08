aaddd55d57af67e82f00d80ec441f4e1b086b74f
ade-ryzki <ade.ryzki@gmail.com>
2022-08-08 15:32:58 +0700

    new DB portfolio

---
+++ models/portfolio.js
@@ -0,0 +1,79 @@
+const db = require('../helpers/db')
+
+
+module.exports = {
+    getPortfolioByUserId: (userId) => {
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT * FROM portfolio WHERE userId='${userId}'`, (error, result) => {
+                if (error) {
+                    reject({
+                        success: false,
+                        message: error.sqlMessage,
+                    })
+                }
+                resolve(result)
+
+            })
+        })
+    },//done
+     getPortfolioById: (portfolioId) => {
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT * FROM portfolio WHERE portfolioId='${portfolioId}'`, (error, result) => {
+                if (error) {
+                    reject({
+                        success: false,
+                        message: error.sqlMessage,
+                    })
+                }
+                resolve(result)
+            })
+        })
+    },//done
+    addPortfolio: async (setData) => {
+        return new Promise((resolve, reject) => {
+            db.query(`INSERT INTO portfolio SET ?`, setData, (err) => {
+                if (err) {
+                    reject({
+                        success: false, message: err.sqlMessage, data: {
+                            errCode: err.code, errNo: err.errno
+                        }
+                    })
+                }
+                resolve({
+                    ...setData
+                  })
+            })
+        })
+    },//done 
+    updatePortfolio: async (setData, portfolioId) => {
+        return new Promise((resolve, reject) => {
+            db.query(`UPDATE portfolio SET ? WHERE portfolioId = ?`, [setData, portfolioId], (err) => {
+                if (err) {
+                    reject({
+                        success: false, message: err.sqlMessage, setData: {
+                            errCode: err.code, errNo: err.errno
+                        }
+                    })
+                }
+                resolve({
+                    portfolioId,
+                    ...setData,
+                })
+            })
+        })
+    },//done 
+    deletePortfolio: async (portfolioId) => {
+        return new Promise((resolve, reject) => {
+            db.query(`DELETE FROM portfolio WHERE portfolioId = ?`, portfolioId, (err, results) => {
+                if (err) {
+                    reject({
+                        success: false, message: err.sqlMessage, data: {
+                            errCode: err.code, errNo: err.errno
+                        }
+                    })
+                }
+                resolve(results)
+            })
+        })
+    }//done 
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express()
 const authRoute = require('./authRoute')
-const { isPekerja, isPerekru, isLogin } = require('../helpers/auth')
+const { isPekerja, isPerekrut, isLogin } = require('../helpers/auth')
+const portfolioRoute = require('./portfolioRoute')
 
 app.use('/auth', authRoute)
+app.use('/portfolio', portfolioRoute)
 
 
 module.exports = app
